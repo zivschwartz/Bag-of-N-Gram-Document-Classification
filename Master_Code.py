87ee899ea5b7d8a9a7b2771571eed875064751c9
Ziv Schwartz <ziv.schwartz@gmail.com>
2018-10-09 01:48:49 -0400

    Update and rename Source_Code.py to Master_Code.py

--- Master_Code.py
+++ Master_Code.py
@@ -299,10 +299,21 @@ print("Test Acc {}".format(test_model(test_loader, model)))
 #Test Acc 80.872
 
 ####################################################################################
-#Varying hyperparameters: Test 1. Vocab Size (Default Vocab Size = 10000)
-#Every other hyperparameter held constant, Vocab Size tested at [20000, 40000, 80000] 
+############################### Model Hyperparameters ##############################
+####################################################################################
 
-#Create the vocabulary of most common 20,000 tokens in the training set.
+#Test 1. Vary N-Gram (Default n = 1)
+#N-Gram n tested at (2,3,4)
+
+
+####################################################################################
+####################################################################################
+
+#Test 2. Vocab Size (Default Vocab Size = 10000)
+#Vocab Size tested at [20000, 40000, 80000] 
+#All other hyperparameter at default values 
+
+#Vocab Size 20000
 max_vocab_size = 20000
 # save index 0 for unk and 1 for pad
 PAD_IDX = 0
@@ -480,7 +491,7 @@ for epoch in range(num_epochs):
             print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
                        epoch+1, num_epochs, i+1, len(train_loader), val_acc))
 
-#Vocab size = 20,000
+#Vocab Size 20000
 print("After training for {} epochs".format(num_epochs))
 print("Val Acc {}".format(test_model(val_loader, model)))
 print("Test Acc {}".format(test_model(test_loader, model)))
@@ -489,7 +500,8 @@ print("Test Acc {}".format(test_model(test_loader, model)))
 #Test Acc 82.008
 
 ####################################################################################
-#Create the vocabulary of most common 40,000 tokens in the training set.
+
+#Vocab Size 40000
 max_vocab_size = 40000
 # save index 0 for unk and 1 for pad
 PAD_IDX = 0
@@ -667,7 +679,7 @@ for epoch in range(num_epochs):
             print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
                        epoch+1, num_epochs, i+1, len(train_loader), val_acc))
             
-#Vocab size 40,000
+#Vocab Size 40000
 print("After training for {} epochs".format(num_epochs))
 print("Val Acc {}".format(test_model(val_loader, model)))
 print("Test Acc {}".format(test_model(test_loader, model)))
@@ -676,7 +688,8 @@ print("Test Acc {}".format(test_model(test_loader, model)))
 #Test Acc 82.316
 
 ####################################################################################
-#Create the vocabulary of most common 80,000 tokens in the training set.
+
+#Vocab Size 80000
 max_vocab_size = 80000
 # save index 0 for unk and 1 for pad
 PAD_IDX = 0
@@ -863,3 +876,367 @@ print("Test Acc {}".format(test_model(test_loader, model)))
 #Test Acc 82.368
 
 ####################################################################################
+####################################################################################
+
+#Test 3. Embedding Size (Default Embedding Size = 100)
+#Embedding Size tested at [25, 50, 150]
+#All other hyperparameters at default values 
+max_vocab_size = 10000
+# save index 0 for unk and 1 for pad
+PAD_IDX = 0
+UNK_IDX = 1
+
+def build_vocab(all_tokens):
+    # Returns:
+    # id2token: list of tokens, where id2token[i] returns token that corresponds to token i
+    # token2id: dictionary where keys represent tokens and corresponding values represent indices
+    token_counter = Counter(all_tokens)
+    vocab, count = zip(*token_counter.most_common(max_vocab_size))
+    id2token = list(vocab)
+    token2id = dict(zip(vocab, range(2,2+len(vocab)))) 
+    id2token = ['<pad>', '<unk>'] + id2token
+    token2id['<pad>'] = PAD_IDX 
+    token2id['<unk>'] = UNK_IDX
+    return token2id, id2token
+
+token2id, id2token = build_vocab(all_train_tokens)
+
+def token2index_dataset(tokens_data):
+    indices_data = []
+    for tokens in tokens_data:
+        index_list = [token2id[token] if token in token2id else UNK_IDX for token in tokens]
+        indices_data.append(index_list)
+    return indices_data
+
+train_data_indices = token2index_dataset(train_data_tokens)
+val_data_indices = token2index_dataset(val_data_tokens)
+test_data_indices = token2index_dataset(test_data_tokens)
+
+#create PyTorch DataLoader
+MAX_SENTENCE_LENGTH = 200
+
+class IMDBDataset(Dataset):
+    """
+    Class that represents a train/validation/test dataset that's readable for PyTorch
+    Note that this class inherits torch.utils.data.Dataset
+    """
+    
+    def __init__(self, data_list, target_list):
+        """
+        @param data_list: list of IMDB tokens 
+        @param target_list: list of IMDB targets 
+
+        """
+        self.data_list = data_list
+        self.target_list = target_list
+        assert (len(self.data_list) == len(self.target_list))
+
+    def __len__(self):
+        return len(self.data_list)
+        
+    def __getitem__(self, key):
+        """
+        Triggered when you call dataset[i]
+        """
+        
+        token_idx = self.data_list[key][:MAX_SENTENCE_LENGTH]
+        label = self.target_list[key]
+        return [token_idx, len(token_idx), label]
+
+def IMDB_collate_func(batch):
+    """
+    Customized function for DataLoader that dynamically pads the batch so that all 
+    data have the same length
+    """
+    data_list = []
+    label_list = []
+    length_list = []
+
+    for datum in batch:
+        label_list.append(datum[2])
+        length_list.append(datum[1])
+    # padding
+    for datum in batch:
+        padded_vec = np.pad(np.array(datum[0]), 
+                                pad_width=((0,MAX_SENTENCE_LENGTH-datum[1])), 
+                                mode="constant", constant_values=0)
+        data_list.append(padded_vec)
+    return [torch.from_numpy(np.array(data_list)), torch.LongTensor(length_list), torch.LongTensor(label_list)]
+
+BATCH_SIZE = 32
+train_dataset = IMDBDataset(train_data_indices, train_target)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           collate_fn=IMDB_collate_func,
+                                           shuffle=True)
+
+val_dataset = IMDBDataset(val_data_indices, val_target)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           collate_fn=IMDB_collate_func,
+                                           shuffle=True)
+
+test_dataset = IMDBDataset(test_data_indices, test_target)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           collate_fn=IMDB_collate_func,
+                                           shuffle=False)
+
+
+#define Bag-of-Words model in PyTorch
+class BagOfWords(nn.Module):
+    """
+    BagOfWords classification model
+    """
+    def __init__(self, vocab_size, emb_dim):
+        """
+        @param vocab_size: size of the vocabulary. 
+        @param emb_dim: size of the word embedding
+        """
+        super(BagOfWords, self).__init__()
+        # pay attention to padding_idx 
+        self.embed = nn.Embedding(vocab_size, emb_dim, padding_idx=0)
+        self.linear = nn.Linear(emb_dim,20)
+    
+    def forward(self, data, length):
+        """
+        
+        @param data: matrix of size (batch_size, max_sentence_length). Each row in data represents a 
+            review that is represented using n-gram index. Note that they are padded to have same length.
+        @param length: an int tensor of size (batch_size), which represents the non-trivial (excludes padding)
+            length of each sentences in the data.
+        """
+        out = self.embed(data)
+        out = torch.sum(out, dim=1)
+        out /= length.view(length.size()[0],1).expand_as(out).float()
+     
+        # return logits
+        out = self.linear(out.float())
+        return out
+
+#Embedding Size 25
+emb_dim = 25
+model = BagOfWords(len(id2token), emb_dim)
+
+learning_rate = 0.01
+num_epochs = 10 # number epoch to train
+
+# Criterion and Optimizer
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Function for testing the model
+def test_model(loader, model):
+    """
+    Help function that tests the model's performance on a dataset
+    @param: loader - data loader for the dataset to test against
+    """
+    correct = 0
+    total = 0
+    model.eval()
+    for data, lengths, labels in loader:
+        data_batch, length_batch, label_batch = data, lengths, labels
+        outputs = F.softmax(model(data_batch, length_batch), dim=1)
+        predicted = outputs.max(1, keepdim=True)[1]
+        
+        total += labels.size(0)
+        correct += predicted.eq(labels.view_as(predicted)).sum().item()
+    return (100 * correct / total)
+
+for epoch in range(num_epochs):
+    for i, (data, lengths, labels) in enumerate(train_loader):
+        model.train()
+        data_batch, length_batch, label_batch = data, lengths, labels
+        optimizer.zero_grad()
+        outputs = model(data_batch, length_batch)
+        loss = criterion(outputs, label_batch)
+        loss.backward()
+        optimizer.step()
+        # validate every 100 iterations
+        if i > 0 and i % 100 == 0:
+            # validate
+            val_acc = test_model(val_loader, model)
+            print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
+                       epoch+1, num_epochs, i+1, len(train_loader), val_acc))
+
+#Embedding Size 25
+print("After training for {} epochs".format(num_epochs))
+print("Val Acc {}".format(test_model(val_loader, model)))
+print("Test Acc {}".format(test_model(test_loader, model)))
+#After training for 10 epochs
+#Val Acc 83.94
+#Test Acc 81.244
+
+####################################################################################
+
+#Embedding Size 50
+emb_dim = 50
+model = BagOfWords(len(id2token), emb_dim)
+
+learning_rate = 0.01
+num_epochs = 10 # number epoch to train
+
+# Criterion and Optimizer
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Function for testing the model
+def test_model(loader, model):
+    """
+    Help function that tests the model's performance on a dataset
+    @param: loader - data loader for the dataset to test against
+    """
+    correct = 0
+    total = 0
+    model.eval()
+    for data, lengths, labels in loader:
+        data_batch, length_batch, label_batch = data, lengths, labels
+        outputs = F.softmax(model(data_batch, length_batch), dim=1)
+        predicted = outputs.max(1, keepdim=True)[1]
+        
+        total += labels.size(0)
+        correct += predicted.eq(labels.view_as(predicted)).sum().item()
+    return (100 * correct / total)
+
+for epoch in range(num_epochs):
+    for i, (data, lengths, labels) in enumerate(train_loader):
+        model.train()
+        data_batch, length_batch, label_batch = data, lengths, labels
+        optimizer.zero_grad()
+        outputs = model(data_batch, length_batch)
+        loss = criterion(outputs, label_batch)
+        loss.backward()
+        optimizer.step()
+        # validate every 100 iterations
+        if i > 0 and i % 100 == 0:
+            # validate
+            val_acc = test_model(val_loader, model)
+            print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
+                       epoch+1, num_epochs, i+1, len(train_loader), val_acc))
+ 
+#Embedding Size 50
+print("After training for {} epochs".format(num_epochs))
+print("Val Acc {}".format(test_model(val_loader, model)))
+print("Test Acc {}".format(test_model(test_loader, model)))
+#After training for 10 epochs
+#Val Acc 83.36
+#Test Acc 80.616
+
+####################################################################################
+
+#Embedding Size 150
+emb_dim = 150
+model = BagOfWords(len(id2token), emb_dim)
+
+learning_rate = 0.01
+num_epochs = 10 # number epoch to train
+
+# Criterion and Optimizer
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Function for testing the model
+def test_model(loader, model):
+    """
+    Help function that tests the model's performance on a dataset
+    @param: loader - data loader for the dataset to test against
+    """
+    correct = 0
+    total = 0
+    model.eval()
+    for data, lengths, labels in loader:
+        data_batch, length_batch, label_batch = data, lengths, labels
+        outputs = F.softmax(model(data_batch, length_batch), dim=1)
+        predicted = outputs.max(1, keepdim=True)[1]
+        
+        total += labels.size(0)
+        correct += predicted.eq(labels.view_as(predicted)).sum().item()
+    return (100 * correct / total)
+
+for epoch in range(num_epochs):
+    for i, (data, lengths, labels) in enumerate(train_loader):
+        model.train()
+        data_batch, length_batch, label_batch = data, lengths, labels
+        optimizer.zero_grad()
+        outputs = model(data_batch, length_batch)
+        loss = criterion(outputs, label_batch)
+        loss.backward()
+        optimizer.step()
+        # validate every 100 iterations
+        if i > 0 and i % 100 == 0:
+            # validate
+            val_acc = test_model(val_loader, model)
+            print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
+                       epoch+1, num_epochs, i+1, len(train_loader), val_acc))
+            
+#Embedding Size 150
+print("After training for {} epochs".format(num_epochs))
+print("Val Acc {}".format(test_model(val_loader, model)))
+print("Test Acc {}".format(test_model(test_loader, model)))
+#After training for 10 epochs
+#Val Acc 83.06
+#Test Acc 80.352
+
+####################################################################################
+############################ Optimization Hyperparameters ##########################
+####################################################################################
+
+#Test 1. Optimizer (Default Optimizer = Adam)
+#Optimizer SGD Test
+#Reset all other hyperparameters
+emb_dim = 100
+model = BagOfWords(len(id2token), emb_dim)
+
+learning_rate = 0.01
+num_epochs = 10 # number epoch to train
+
+# Criterion and Optimizer
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Function for testing the model
+def test_model(loader, model):
+    """
+    Help function that tests the model's performance on a dataset
+    @param: loader - data loader for the dataset to test against
+    """
+    correct = 0
+    total = 0
+    model.eval()
+    for data, lengths, labels in loader:
+        data_batch, length_batch, label_batch = data, lengths, labels
+        outputs = F.softmax(model(data_batch, length_batch), dim=1)
+        predicted = outputs.max(1, keepdim=True)[1]
+        
+        total += labels.size(0)
+        correct += predicted.eq(labels.view_as(predicted)).sum().item()
+    return (100 * correct / total)
+
+for epoch in range(num_epochs):
+    for i, (data, lengths, labels) in enumerate(train_loader):
+        model.train()
+        data_batch, length_batch, label_batch = data, lengths, labels
+        optimizer.zero_grad()
+        outputs = model(data_batch, length_batch)
+        loss = criterion(outputs, label_batch)
+        loss.backward()
+        optimizer.step()
+        # validate every 100 iterations
+        if i > 0 and i % 100 == 0:
+            # validate
+            val_acc = test_model(val_loader, model)
+            print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
+                       epoch+1, num_epochs, i+1, len(train_loader), val_acc))
+
+#Optimizer SGD
+print("After training for {} epochs".format(num_epochs))
+print("Val Acc {}".format(test_model(val_loader, model)))
+print("Test Acc {}".format(test_model(test_loader, model)))
+#After training for 10 epochs
+#Val Acc 64.54
+#Test Acc 63.72
+
+####################################################################################
+####################################################################################
+
+#Test 2. Learning Rate (Default = 0.01)
