11ec82c1509307a5cebf37ff69cc2df96c7ef4b3
Ziv Schwartz <ziv.schwartz@gmail.com>
2018-10-09 03:51:14 -0400

    Update Master_Code.py

--- Master_Code.py
+++ Master_Code.py
@@ -1399,4 +1399,63 @@ print("Test Acc {}".format(test_model(test_loader, model)))
 #Test Acc 79.82
 
 ####################################################################################
+####################################################################################
 
+#Test 3. Learning Rate Schedule
+#By Default Learning Rate is reduced linearly over the course of training.
+#Reset all other hyperparameters
+#Create a Non-Linear Learning Rate Schedule. Testing Exponential Learning Rate Schedule
+
+learning_rate = 0.01
+num_epochs = 10 # number epoch to train
+
+# Criterion and Optimizer
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+#Creates Exponential Learning Rate Schedule
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.1)
+
+# Function for testing the model
+def test_model(loader, model):
+    """
+    Help function that tests the model's performance on a dataset
+    @param: loader - data loader for the dataset to test against
+    """
+    correct = 0
+    total = 0
+    model.eval()
+    for data, lengths, labels in loader:
+        data_batch, length_batch, label_batch = data, lengths, labels
+        outputs = F.softmax(model(data_batch, length_batch), dim=1)
+        predicted = outputs.max(1, keepdim=True)[1]
+        
+        total += labels.size(0)
+        correct += predicted.eq(labels.view_as(predicted)).sum().item()
+    return (100 * correct / total)
+
+for epoch in range(num_epochs):
+    #pass the scheduler parameter within the epoch loop
+    scheduler.step()
+    for i, (data, lengths, labels) in enumerate(train_loader):
+        model.train()
+        data_batch, length_batch, label_batch = data, lengths, labels
+        optimizer.zero_grad()
+        outputs = model(data_batch, length_batch)
+        loss = criterion(outputs, label_batch)
+        loss.backward()
+        optimizer.step()
+        # validate every 100 iterations
+        if i > 0 and i % 100 == 0:
+            # validate
+            val_acc = test_model(val_loader, model)
+            print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format( 
+                       epoch+1, num_epochs, i+1, len(train_loader), val_acc))
+            
+#Exponential Learning Rate
+print("After training for {} epochs".format(num_epochs))
+print("Val Acc {}".format(test_model(val_loader, model)))
+print("Test Acc {}".format(test_model(test_loader, model)))
+#After training for 10 epochs
+#Val Acc 83.3
+#Test Acc 81.052
