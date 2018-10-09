b79d9e9ef1c2b22ee4df85eb19c41a0e586c4f66
Ziv Schwartz <ziv.schwartz@gmail.com>
2018-10-08 20:54:46 -0400

    Update and rename Source Code.py to Source_Code.py

---
+++ Source_Code.py
@@ -0,0 +1,304 @@
+#Loading the data
+import pandas as pd
+import numpy as np
+import random
+from os import listdir
+
+#Load single .txt file
+def load_doc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
+ 
+#Load all .txt files in the directory
+def process_docs(directory, data):
+    i = 0
+    targetdata = []
+    for target in ('pos', 'neg'):
+        for filename in listdir(directory + target):
+            #Attributing the correct sentiment labels
+            if target == 'pos':
+                target_int = 1
+            else:
+                target_int = 0
+            #Including only relevant .txt files
+            if not filename.endswith(".txt"):
+                continue
+            #Build the full path of the file to open
+            path = directory + target + '/' + filename
+            #Load document and label target
+            data.append(load_doc(path))
+            i += 1
+            targetdata.append(target_int)
+    return data, targetdata
+
+#Paths to training and testing directories
+train_dir = '/Users/zivschwartz1/IMDB/train/'
+test_dir = '/Users/zivschwartz1/IMDB/test/'
+
+train = []
+test = []
+
+#Create full train/test lists and their target lists
+full_train, full_train_target = process_docs(train_dir, train)
+test, test_target = process_docs(test_dir, test)
+
+#Combining train and target and shuffling data for random sampling
+train_combined = list(zip(full_train, full_train_target))
+random.shuffle(train_combined)
+full_train[:], full_train_target[:] = zip(*train_combined)
+
+#Combining train and target and shuffling data for random sampling
+test_combined = list(zip(test, test_target))
+random.shuffle(train_combined)
+test[:], test_target[:] = zip(*test_combined)
+
+#Split full train/target into sub-training and validation sets
+#20000 train/target values
+train = full_train[:20000]
+train_target = full_train_target[:20000]
+
+#5000 validation/target values
+val = full_train[20000:]
+val_target = full_train_target[20000:]
+
+import spacy
+import string
+import pickle as pkl
+
+#Load English tokenizer, tagger, parser, NER and word vectors
+tokenizer = spacy.load('en_core_web_sm')
+punctuations = string.punctuation
+
+#Make lowercase and remove punctuation
+def tokenize(sent):
+  tokens = tokenizer(sent)
+  return [token.text.lower() for token in tokens if (token.text not in punctuations)]
+
+def tokenize_dataset(dataset):
+    token_dataset = []
+    # we are keeping track of all tokens in dataset 
+    # in order to create vocabulary later
+    all_tokens = []
+    
+    for sample in dataset:
+        tokens = tokenize(sample)
+        token_dataset.append(tokens)
+        all_tokens += tokens
+
+    return token_dataset, all_tokens
+
+#Validation set tokens
+print("Tokenizing val data")
+val_data_tokens, _ = tokenize_dataset(val)
+pkl.dump(val_data_tokens, open("val_data_tokens.p", "wb"))
+
+#Test set tokens
+print("Tokenizing test data")
+test_data_tokens, all_test_tokens = tokenize_dataset(test)
+pkl.dump(test_data_tokens, open("test_data_tokens.p", "wb"))
+
+#Train set tokens
+print("Tokenizing train data")
+train_data_tokens, all_train_tokens = tokenize_dataset(train)
+pkl.dump(train_data_tokens, open("train_data_tokens.p", "wb"))
+pkl.dump(all_train_tokens, open("all_train_tokens.p", "wb"))
+
+#Loading train, validation, test and all token files
+train_data_tokens = pkl.load(open("train_data_tokens.p", "rb"))
+val_data_tokens = pkl.load(open("val_data_tokens.p", "rb"))
+test_data_tokens = pkl.load(open("test_data_tokens.p", "rb"))
+all_train_tokens = pkl.load(open("all_train_tokens.p", "rb"))
+
+#Create the vocabulary of most common 10,000 tokens in the training set
+from collections import Counter
+
+max_vocab_size = 10000
+#Save index 0 for unk and 1 for pad
+PAD_IDX = 0
+UNK_IDX = 1
+
+def build_vocab(all_tokens):
+    # Returns:
+    # id2token: list of tokens, where id2token[i] returns token that corresponds to token i
+    # token2id: dictionary where keys represent tokens and corresponding values represent indices
+    token_counter = Counter(all_tokens)
+    vocab, count = zip(*token_counter.most_common(max_vocab_size))
+    id2token = list(vocab)
+    token2id = dict(zip(vocab, range(2,2+len(vocab)))) 
+    id2token = ['<pad>', '<unk>'] + id2token
+    token2id['<pad>'] = PAD_IDX 
+    token2id['<unk>'] = UNK_IDX
+    return token2id, id2token
+
+token2id, id2token = build_vocab(all_train_tokens)
+
+#Convert token to id in the dataset
+def token2index_dataset(tokens_data):
+    indices_data = []
+    for tokens in tokens_data:
+        index_list = [token2id[token] if token in token2id else UNK_IDX for token in tokens]
+        indices_data.append(index_list)
+    return indices_data
+
+train_data_indices = token2index_dataset(train_data_tokens)
+val_data_indices = token2index_dataset(val_data_tokens)
+test_data_indices = token2index_dataset(test_data_tokens)
+
+#create PyTorch DataLoader
+MAX_SENTENCE_LENGTH = 200
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class IMDBDataset(Dataset):
+    """
+    Class that represents a train/validation/test dataset that's readable for PyTorch
+    Note that this class inherits torch.utils.data.Dataset
+    """
+    
+    def __init__(self, data_list, target_list):
+        """
+        @param data_list: list of IMDB tokens 
+        @param target_list: list of IMDB targets 
+
+        """
+        self.data_list = data_list
+        self.target_list = target_list
+        assert (len(self.data_list) == len(self.target_list))
+
+    def __len__(self):
+        return len(self.data_list)
+        
+    def __getitem__(self, key):
+        """
+        Triggered when you call dataset[i]
+        """
+        
+        token_idx = self.data_list[key][:MAX_SENTENCE_LENGTH]
+        label = self.target_list[key]
+        return [token_idx, len(token_idx), label]
+
+def IMDB_collate_func(batch):
+    """
+    Customized function for DataLoader that dynamically pads the batch so that all 
+    data have the same length
+    """
+    data_list = []
+    label_list = []
+    length_list = []
+    for datum in batch:
+        label_list.append(datum[2])
+        length_list.append(datum[1])
+    # padding
+    for datum in batch:
+        padded_vec = np.pad(np.array(datum[0]), 
+                                pad_width=((0,MAX_SENTENCE_LENGTH-datum[1])), 
+                                mode="constant", constant_values=0)
+        data_list.append(padded_vec)
+    return [torch.from_numpy(np.array(data_list)), torch.LongTensor(length_list), torch.LongTensor(label_list)]
+
+BATCH_SIZE = 32
+train_dataset = IMDBDataset(train_data_indices, train_target)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           collate_fn=IMDB_collate_func,
+                                           shuffle=True)
+
+val_dataset = IMDBDataset(val_data_indices, val_target)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           collate_fn=IMDB_collate_func,
+                                           shuffle=True)
+
+test_dataset = IMDBDataset(test_data_indices, test_target)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                           batch_size=BATCH_SIZE,
+                                           collate_fn=IMDB_collate_func,
+                                           shuffle=False)
+
+#First import torch related libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#Define Bag-of-Words model in PyTorch
+class BagOfWords(nn.Module):
+    """
+    BagOfWords classification model
+    """
+    def __init__(self, vocab_size, emb_dim):
+        """
+        @param vocab_size: size of the vocabulary. 
+        @param emb_dim: size of the word embedding
+        """
+        super(BagOfWords, self).__init__()
+        # pay attention to padding_idx 
+        self.embed = nn.Embedding(vocab_size, emb_dim, padding_idx=0)
+        self.linear = nn.Linear(emb_dim,20)
+    
+    def forward(self, data, length):
+        """
+        
+        @param data: matrix of size (batch_size, max_sentence_length). Each row in data represents a 
+            review that is represented using n-gram index. Note that they are padded to have same length.
+        @param length: an int tensor of size (batch_size), which represents the non-trivial (excludes padding)
+            length of each sentences in the data.
+        """
+        out = self.embed(data)
+        out = torch.sum(out, dim=1)
+        out /= length.view(length.size()[0],1).expand_as(out).float()
+     
+        # return logits
+        out = self.linear(out.float())
+        return out
+
+emb_dim = 100
+model = BagOfWords(len(id2token), emb_dim)
+
+
+learning_rate = 0.01
+num_epochs = 10 # number epoch to train
+
+# Criterion and Optimizer
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Function for testing the model
+def test_model(loader, model):
+    """
+    Help function that tests the model's performance on a dataset
+    @param: loader - data loader for the dataset to test against
+    """
+    correct = 0
+    total = 0
+    model.eval()
+    for data, lengths, labels in loader:
+        data_batch, length_batch, label_batch = data, lengths, labels
+        outputs = F.softmax(model(data_batch, length_batch), dim=1)
+        predicted = outputs.max(1, keepdim=True)[1]
+        
+        total += labels.size(0)
+        correct += predicted.eq(labels.view_as(predicted)).sum().item()
+    return (100 * correct / total)
+
+for epoch in range(num_epochs):
+    for i, (data, lengths, labels) in enumerate(train_loader):
+        model.train()
+        data_batch, length_batch, label_batch = data, lengths, labels
+        optimizer.zero_grad()
+        outputs = model(data_batch, length_batch)
+        loss = criterion(outputs, label_batch)
+        loss.backward()
+        optimizer.step()
+        # validate every 100 iterations
+        if i > 0 and i % 100 == 0:
+            # validate
+            val_acc = test_model(val_loader, model)
+            print('Epoch: [{}/{}], Step: [{}/{}], Validation Acc: {}'.format(epoch+1, num_epochs, i+1, len(train_loader), val_acc))
+
+print("After training for {} epochs".format(num_epochs))
+print("Val Acc {}".format(test_model(val_loader, model)))
+print("Test Acc {}".format(test_model(test_loader, model)))
